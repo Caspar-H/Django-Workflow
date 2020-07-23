69d09b68069a6732a67c7461e91a9968e785ba18
unknown <casparhpf@gmail.com>
2020-07-24 09:24:33 +1000

    site documents upload/download done, better UI

--- fileupload/urls.py
+++ fileupload/urls.py
@@ -6,8 +6,10 @@ app_name = 'fileupload'
 
 urlpatterns = [
     path('documents_folder/', views.documents_folder, name='documents_folder'),
+    path('documents_folder_v2/', views.documents_folder_v2, name='documents_folder_v2'),
     path('documents_download/', views.documents_download, name='documents_download'),
     # path('documents_download_v2/', views.documents_download_v2, name='documents_download_v2'),
 
     path('documents_upload/', views.documents_upload, name='documents_upload'),
+    # path('documents_upload_v2/', views.documents_upload_v2, name='documents_upload_v2'),
 ]
--- fileupload/views.py
+++ fileupload/views.py
@@ -6,7 +6,7 @@ from minio import Minio, ResponseError
 
 import tkinter
 import tkinter.messagebox as tkmb
-from tkinter.filedialog import askdirectory
+from tkinter.filedialog import askdirectory, askopenfilename
 
 import os
 
@@ -19,6 +19,7 @@ bucket_name = 'siteinfo'
 
 # Create your views here.
 def documents_folder(request):
+    # documents_list(request)
     if request.method == 'GET':
         # Get object list
         objects = minioClient.list_objects(bucket_name, prefix=request.GET.get('prefix', ''), recursive=False)
@@ -30,10 +31,15 @@ def documents_folder(request):
             # print(obj.bucket_name, obj.object_name.encode('utf-8'), obj.last_modified,
             #       obj.etag, obj.size, obj.content_type, obj.is_dir)
             if obj.is_dir:
-                folder_list.append({'folder_name': obj.object_name.split('/')[-2], 'object_name': obj.object_name})
+                folder_list.append({'folder_name': obj.object_name.split('/')[-2], 'object_name': obj.object_name,
+                                    })
             else:
+                if obj.size / 1024 < 1024:
+                    file_size = str(round(obj.size / 1024, 2)) + 'KB'
+                else:
+                    file_size = str(round(obj.size / 1024 / 1024, 2)) + 'MB'
                 file_list.append({'file_name': obj.object_name.split('/')[-1], 'last_modified': obj.last_modified,
-                                  'object_name': obj.object_name})
+                                  'object_name': obj.object_name, 'size': file_size})
         # print(folder_list)
         # print(file_list)
         return render(request, 'fileupload/documents_folder.html', locals())
@@ -96,56 +102,86 @@ def documents_download(request):
         print(err)
 
 
-def documents_download_v2(request):
+def documents_upload(request):
+    file_path = request.GET.get('prefix', '')
+
     try:
         root = tkinter.Tk()
         root.withdraw()
+        file_location = askopenfilename()
+        file_name = file_location.split('/')[-1]
 
-        path = askdirectory(title='Select Folder')  # shows dialog box and return the path
-        object_name = request.GET.get('object_name')
-        file_dir = path + '/' + object_name.split('/')[-1]
+        # If file is not selected, it will jump back to the folder.
+        if not file_location:
+            response = redirect('fileupload:documents_folder')
+            response['Location'] += '?prefix={}'.format(file_path)
+            root.destroy()
+            return response
 
-        file_path = '/'.join(object_name.split('/')[:-1]) + '/'
-        if file_path == '/':
-            file_path = ''
+        # Check if file-name exists in the Minio Server
+        objects_all = minioClient.list_objects(bucket_name, prefix=request.GET.get('prefix', ''), recursive=False)
+        file_list = []
+        current_path = prefix = request.GET.get('prefix', '')
+        for obj in objects_all:
+            if not obj.is_dir:
+                file_list.append(obj.object_name.split('/')[-1])
+        if file_name in file_list:
+            is_replace = tkmb.askyesno("Replace or Skip Files",
+                                       "The File Server already has a file named {} in this folder, do you want to "
+                                       "replace it?".format(file_name))
+            if is_replace:
+                minio_object_name = file_path + file_name if file_path else file_name
 
-        print(minioClient.fget_object(bucket_name, object_name, file_dir))
-        root.destroy()
+                root.destroy()
+                print(minioClient.fput_object(bucket_name, minio_object_name, file_location))
 
-        response = redirect('fileupload:documents_folder')
-        response['Location'] += '?prefix={}'.format(file_path)
+                response = redirect('fileupload:documents_folder')
+                response['Location'] += '?prefix={}'.format(file_path)
 
-        return response
+                return response
+
+            else:
+                root.destroy()
+                response = redirect('fileupload:documents_folder')
+                response['Location'] += '?prefix={}'.format(file_path)
+                return response
+        else:
+
+            minio_object_name = file_path + file_name if file_path else file_name
+
+            root.destroy()
+            print(minioClient.fput_object(bucket_name, minio_object_name, file_location))
+
+            response = redirect('fileupload:documents_folder')
+            response['Location'] += '?prefix={}'.format(file_path)
+
+            return response
 
     except ResponseError as err:
         print(err)
-
-
-def documents_upload(request):
-    file_path = request.POST.get('current_path')
-    print(request.POST)
-    if request.method == "POST":
-        try:
-            selected_file = request.FILES["selected_file"]
-            selected_file_name = request.FILES["selected_file"].name
-            print(selected_file)
-            print(file_path)
-
-            try:
-                with open(selected_file, 'rb') as file_data:
-                    file_stat = os.stat('my-testfile')
-                    print(minioClient.put_object(bucket_name, file_path+'/'+selected_file_name,
-                                                 file_data, file_stat.st_size))
-            except ResponseError as err:
-                print(err)
-
-        except Exception as e:
-            print(e)
-            print("Unable to upload file. ")
+        print("Unable to upload file. ")
 
         response = redirect('fileupload:documents_folder')
         response['Location'] += '?prefix={}'.format(file_path)
 
         return response
-    else:
-        return HttpResponse("Only POST request is accepted")
+
+
+# def documents_list(request):
+#     print('Documents List 1:')
+#     objects_1 = minioClient.list_objects_v2(bucket_name, prefix='',
+#                                           recursive=False, start_after='')
+#     for obj in objects_1:
+#         print(obj.bucket_name, obj.object_name.encode('utf-8'), obj.last_modified,
+#               obj.etag, obj.size, obj.content_type)
+#
+#     print('Documents List 2:')
+#
+#     objects_2 = minioClient.list_objects(bucket_name, prefix=request.GET.get('prefix', ''), recursive=True)
+#     for obj in objects_2:
+#         print(obj.bucket_name, obj.object_name.encode('utf-8'), obj.last_modified,
+#               obj.etag, obj.size, obj.content_type)
+#
+#     return None
+def documents_folder_v2(request):
+    return render(request, 'fileupload/documents_folder_v2.html', locals())
